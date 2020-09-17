2e82bc9a3ece33366ae9b1d58e794b4a2ae2dda0
Ubuntu <ubuntu@ip-172-31-11-98.us-west-2.compute.internal>
2020-09-17 16:03:00 +0000

    removed output value

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "strongdm_gateway_ips" {
-  value       = module.sdm.sdm_gateway_ips != null ? module.sdm.sdm_gateway_ips : ["No Gateways Found"]
+  value       = module.sdm.sdm_gateway_ips != null ? module.sdm.sdm_gateway_ips : null
   description = "The IP addresses assigned to your strongDM gateways"
 }
