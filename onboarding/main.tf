f9cfb7dd20f68e8db052c62e59b1cb5d381ff4f3
Guido Dobboletta <guidodobboletta@gmail.com>
2021-06-02 17:06:33 -0500

    move http resource into a separate submodule and merge providers.tf into main.tf

--- onboarding/main.tf
+++ onboarding/main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_version = ">= 0.14.0"
+  required_providers {
+    aws = ">= 3.0.0"
+    sdm = {
+      source  = "strongdm/sdm"
+      version = ">= 1.0.15"
+    }
+    random     = ">= 2.0.0"
+    local      = ">= 1.0.0"
+    null       = ">= 2.0.0"
+    kubernetes = ">= 1.11.0"
+    template   = ">= 2.1.0"
+  }
+}
+
 module "windows_server" {
   count          = var.create_rdp ? 1 : 0
   source         = "./windows_server"
@@ -32,6 +48,21 @@ module "mysql" {
   default_tags   = local.default_tags
   security_group = module.sdm.gateway_security_group_id
   subnet_ids     = local.subnet_ids
+  admins_id      = sdm_role.admins.id
+  read_only_id   = sdm_role.read_only.id
+}
+
+module "http_website" {
+  count          = var.create_http ? 1 : 0
+  source         = "./http"
+  create_ssh     = var.create_ssh
+  prefix         = var.prefix
+  vpc_id         = local.vpc_id
+  tags           = var.tags
+  default_tags   = local.default_tags
+  security_group = module.sdm.gateway_security_group_id
+  subnet_ids     = local.subnet_ids
+  ssh_pubkey     = data.sdm_ssh_ca_pubkey.this_key.public_key
   admins_id      = sdm_role.admins.id
   read_only_id   = sdm_role.read_only.id
 }
