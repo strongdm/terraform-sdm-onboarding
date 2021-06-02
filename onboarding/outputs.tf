8a4a8519ac4d500ce2f80b4ebe967c60d43818f8
Guido Dobboletta <guidodobboletta@gmail.com>
2021-06-02 17:52:27 -0500

    remove default outputs

--- onboarding/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "strongdm_gateway_ips" {
-  value       = module.sdm.sdm_gateway_ips != null ? module.sdm.sdm_gateway_ips : null
-  description = "The IP addresses assigned to your strongDM gateways"
-}
