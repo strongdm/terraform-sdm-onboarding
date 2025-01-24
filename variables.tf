260f22e218d3cd7d39e2df528864d9eeff18a852
nitrocode <7775707+nitrocode@users.noreply.github.com>
2025-01-24 12:21:50 -0600

    feat: add subnet_ids
    Signed-off-by: nitrocode <7775707+nitrocode@users.noreply.github.com>

--- variables.tf
+++ variables.tf
@@ -2,29 +2,33 @@
 variable "SDM_API_ACCESS_KEY" {
   type      = string
   sensitive = true
+  default = null
 }
 
 variable "SDM_API_SECRET_KEY" {
   type      = string
   sensitive = true
+  default = null
 }
 
 variable "SDM_ADMINS_EMAILS" {
   type = string
+  default = null
 }
 
 ### AWS ###
 variable "AWS_ACCESS_KEY_ID" {
   type      = string
   sensitive = true
+  default = null
 }
 
 variable "AWS_SECRET_ACCESS_KEY" {
   type      = string
   sensitive = true
+  default = null
 }
 
 variable "REGION_AWS" {
-  type    = string
-  default = null
+  type = string
 }
