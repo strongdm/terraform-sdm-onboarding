07ccda51d22dc725c0721debc6a523799d2c190a
Lance Haig <lance.haig@strongdm.com>
2025-05-02 10:16:26 +0200

    getting the code running
    Fix missing pieces to get the code deployment working

--- onboarding/versions.tf
+++ onboarding/versions.tf
@@ -1,3 +1,8 @@
+# AWS Provider - Uses the region variable and standard AWS authentication methods
+provider "aws" {
+  region = var.region # Region can be specified in terraform.tfvars
+}
+
 terraform {
   required_version = ">= 0.14.0"
   required_providers {
